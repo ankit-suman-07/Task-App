c25873dd668806941597ed52546ca2d47d49edf5
ankit-suman-07 <ankitsuman07@gmail.com>
2024-10-17 01:28:30 +0530

    implemented function to create a new task

--- src/app/new-task/page.tsx
+++ src/app/new-task/page.tsx
@@ -1,5 +1,22 @@
 import React from 'react'
 import Link from 'next/link'
+import { prisma } from '@/db'
+import { redirect } from 'next/navigation'
+
+async function createTodo(data: FormData) {
+    "use server"
+
+    const title = data.get('title')
+    if(typeof title !== 'string' || title.length === 0) {
+        throw new Error('Invalid Title');
+    }
+
+    await prisma.todo.create({
+        data: {title, complete: false}
+    });
+
+    redirect("/");
+}
 
 const NewTask = () => {
   return (

dcabd7d718dc7e0ee4fcf7fb3a7d167d2432338d
ankit-suman-07 <ankitsuman07@gmail.com>
2024-10-17 01:44:42 +0530

    Completed added types for props

--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
-const ToDoItem = () => {
+type ToDoItemProps = {
+    id: string,
+    title: string,
+    complete: boolean,
+    toggleToDo: (id: string, complete: boolean) => void,
+    deleteToDo: (id: string) => void
+}
+
+const ToDoItem = ({id, title, complete, toggleToDo, deleteToDo}: ToDoItemProps) => {
   return (
     <div>
         <input type='checkbox' />

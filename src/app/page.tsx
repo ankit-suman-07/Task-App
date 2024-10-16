ad4f7f15c272315840b794ebdf997a99290ca724
ankit-suman-07 <ankitsuman07@gmail.com>
2024-10-17 00:53:39 +0530

    setup prisma ORM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       Main Page
+      
     </div>
   );
 }

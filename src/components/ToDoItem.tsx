d2e1ceb9b7b5a8ca079c1675bcbd315b26c37d76
ankit-suman-07 <ankitsuman07@gmail.com>
2024-10-17 01:10:27 +0530

    created component to display each item

---
+++ src/components/ToDoItem.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ToDoItem = () => {
+  return (
+    <div>
+        <input type='checkbox' />
+        <label>Item Name</label>
+        <button>Delete</button>
+    </div>
+  )
+}
+
+export default ToDoItem

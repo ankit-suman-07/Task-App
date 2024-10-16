22e297e9d6552ad1a2b217553e5a281ae5b33652
ankit-suman-07 <ankitsuman07@gmail.com>
2024-10-17 01:40:42 +0530

    implemented methods to delete and update tasks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,42 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import ToDoItem from "@/components/ToDoItem";
+import { prisma } from "@/db";
 
-export default function Home() {
+function getTodos() {
+  return prisma.todo.findMany();
+}
+
+async function toggleToDo(id: string, complete:boolean) {
+  "use server"
+
+  await prisma.todo.update({where: {id}, data: {complete}});
+}
+
+async function deleteToDo(id: string) {
+  "use server"
+
+  await prisma.todo.delete({where: {id}});
+}
+
+export default async function Home() {
+
+  const todos = await getTodos();
   return (
     <div>
-      Main Page
-      
+      <header>
+        <div>Task-aPP</div>
+        <Link href='/new-task' >New Task</Link>
+      </header>
+      <main>
+        <h2>All tasks</h2>
+        {
+          todos.map((todo) => (
+            <ToDoItem key={todo.id} {...todo} toggleToDo={toggleToDo} deleteToDo={deleteToDo} />
+          ))
+        }
+      </main>
     </div>
   );
 }
